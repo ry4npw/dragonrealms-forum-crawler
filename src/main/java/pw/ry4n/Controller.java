5e007021e6d1c5097180acd7187a212e8d04cc97
Ryan Powell <mrpowell@gmail.com>
2018-01-04 12:04:16 -0500

    working crawler

---
+++ src/main/java/pw/ry4n/Controller.java
@@ -0,0 +1,39 @@
+package pw.ry4n;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class Controller {
+	public static void main(String[] args) throws Exception {
+		String crawlStorageFolder = "target/data/crawl/root";
+		int numberOfCrawlers = 7;
+
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder(crawlStorageFolder);
+		config.setResumableCrawling(true);
+
+		/*
+		 * Instantiate the controller for this crawl.
+		 */
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+		/*
+		 * For each crawl, you need to add some seed urls. These are the first
+		 * URLs that are fetched and then the crawler starts following links
+		 * which are found in these pages
+		 */
+		controller.addSeed("http://forums.play.net/return_from_pdn?username=&tcode=20180104104244&authentication=&num=17218&type=legacy");
+
+		/*
+		 * Start the crawl. This is a blocking operation, meaning that your code
+		 * will reach the line after this only when crawling is finished.
+		 */
+		controller.start(ForumCrawler.class, numberOfCrawlers);
+	}
+}
---
+++ src/main/java/pw/ry4n/ForumCrawler.java
@@ -0,0 +1,46 @@
+package pw.ry4n;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class ForumCrawler extends WebCrawler {
+	/**
+	 * This method receives two parameters. The first parameter is the page in
+	 * which we have discovered this new url and the second parameter is the new
+	 * url. You should implement this function to specify whether the given url
+	 * should be crawled or not (based on your crawling logic). In this example,
+	 * we are instructing the crawler to ignore urls that have css, js, git, ...
+	 * extensions and to only accept urls that start with
+	 * "http://www.ics.uci.edu/". In this case, we didn't need the referringPage
+	 * parameter to make the decision.
+	 */
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		return (href.startsWith("http://forums.play.net/forums/dragonrealms/")
+				|| href.startsWith("http://www.play.net/remote/validation.asp")
+				|| href.startsWith("https://www.play.net/remote/validation.asp")
+				|| href.startsWith("http://forums.play.net/return_from_pdn")
+				|| href.equals("http://forums.play.net/forums"))
+				&& !href.contains("/thread/");
+	}
+
+	/**
+	 * This function is called when a page is fetched and ready to be processed
+	 * by your program.
+	 */
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("URL: " + url);
+
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String html = htmlParseData.getHtml();
+
+			System.out.println("Html length: " + html.length());
+		}
+	}
+}
