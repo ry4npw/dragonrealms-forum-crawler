9bf199cb41a8ce79c8e251f68327f4bbc5c0f168
Ryan Powell <mrpowell@gmail.com>
2018-01-04 11:05:28 -0500

    initial commit

---
+++ src/test/java/pw/ry4n/AppTest.java
@@ -0,0 +1,18 @@
+package pw.ry4n;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
