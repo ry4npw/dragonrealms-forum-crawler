50b877c64bd5751a2249eea60586bd65f2568c7c
Ryan Powell <mrpowell@gmail.com>
2018-01-05 13:41:09 -0500

    fix logic in shouldVisit().

--- src/main/java/pw/ry4n/crawler/ForumCrawler.java
+++ src/main/java/pw/ry4n/crawler/ForumCrawler.java
@@ -32,19 +32,23 @@ public class ForumCrawler extends WebCrawler {
 		String href = url.getURL().toLowerCase();
 		return (
 		// visit the authentication pages to get the required cookies
-		href.contains("www.play.net/remote/validation.asp") || href.startsWith("http://forums.play.net/return_from_pdn")
-		// the return from authentication to takes you to the main forums page
+		(href.contains("www.play.net/remote/validation.asp")
+				|| href.startsWith("http://forums.play.net/return_from_pdn")
+				// the return from authentication to takes you to the main
+				// forums page
 				|| href.equals("http://forums.play.net/forums"))
 				// crawl all of the DragonRealms forums
-				|| href.startsWith("http://forums.play.net/forums/dragonrealms/")
-						// don't follow a single post. we want to only visit
-						// "pages" of posts
-						&& !FILTER.matcher(href).find()
-						// don't visit thread pages. we want to crawl the legacy
-						// views to minimize duplicates.
-						&& !href.contains("/thread/")
-						// don't visit reply links
-						&& !href.contains("reply_to=");
+				|| href.startsWith("http://forums.play.net/forums/dragonrealms/"))
+				// don't follow a single post. we want to only visit "pages" of
+				// posts
+				&& !FILTER.matcher(href).find()
+				// don't visit thread pages. we want to crawl the legacy views
+				// to minimize duplicates.
+				&& !href.contains("/thread/")
+				// don't visit reply links
+				&& !href.contains("reply_to=")
+				// page 1 is the same result as /view so don't re-visit it
+				&& !href.endsWith("&page=1");
 	}
 
 	/**
